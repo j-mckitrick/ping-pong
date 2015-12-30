fafcffab60c93ed3a6ef21e269d4da2293dd2ce3
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-30 14:15:43 -0500

    Add seed data and show on home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @players = Player.all.order(grade: :desc)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Player.create(name: 'Jonathon', email: 'jmckitrick@gmail.com', hand: 'right', grade: 50)
+Player.create(name: 'Matt', email: 'jmckitrick+matt@gmail.com', hand: 'left', grade: 75)
+Player.create(name: 'Chot', email: 'jmckitrick+chot@gmail.com', hand: 'right', grade: 90)
+Player.create(name: 'Jake', email: 'jmckitrick+jake@gmail.com', hand: 'right', grade: 25)
